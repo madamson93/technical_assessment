c7379965ace44db3d4fb9d834858e9b1b8a4cb17
Monique Adamson <madamson@hotmail.co.uk>
2015-03-01 18:53:30 +0000

    Scoreboard setup
    Function created to save players to multi dimensional array

--- BowlingGame.php
+++ BowlingGame.php
@@ -32,4 +32,23 @@ class BowlingGame {
      */
     public $Shots = array();
 
+    /**
+     * Ask for user input to loop through and ask the names of players playing a bowling game
+     */
+    function SavePlayers() {
+        Print("Please write in the number of players");
+        /** @var int $NumberofPlayers
+        Local variable to save the number of players playing the game */
+        $NumberofPlayers = "";
+
+        if($NumberofPlayers > 6){
+            Print("You can only have six players, please try again.");
+            SavePlayers();
+        } else {
+            for ($i = 0; $i <= $this->$NumberofPlayers; $i++){
+                $this->Players[$i]["name"] = "";
+            }
+        }
+    }
+
 }
