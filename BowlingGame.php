10326223791b460db01bb65df5c8cce98ea8700c
Monique Adamson <madamson@hotmail.co.uk>
2015-03-02 23:55:45 +0000

    Functions added
    Functions added to add on more opportunities for players if they score a strike or spare

--- BowlingGame.php
+++ BowlingGame.php
@@ -5,9 +5,7 @@
  * Date: 18/02/15
  * Time: 14:28
  */
-
 class BowlingGame {
-
     /**
      * @var int
      * Ultimate number of frames per bowling game
@@ -24,13 +22,7 @@ class BowlingGame {
      * @var array
      * two dimensional array to save players and scores
      */
-    public $Players = array(
-        "name" => array (
-            "score" => array(
-            )
-        )
-    );
-
+    public $Players = array(        "name" => array (            "score" => array(            )        )    );
 
     /**
      * @var array
@@ -43,40 +35,64 @@ class BowlingGame {
      */
     function SavePlayers() {
         Print("Please write in the number of players");
-          if($this->NumberofPlayers > 6){
+
+        if($this->NumberofPlayers > 6){
             Print("You can only have six players, please try again.");
             SavePlayers();
         } else {
             for ($i = 0; $i <= $this->NumberofPlayers; $i++){
                 $this->Players[$i]["name"] = "";
             }
+
         }
+
     }
 
+    /**
+     * @param int $pins
+     * Row in the bowled array will be the number of pins bowled over by a player
+     */
+    function Bowl(int $pins){
+        $Shots[] = $pins;
+    }
 
     /**
      * Function to calculate score and add bonus points
      */
     function Score(){
-
         //current shot that player is on
         $currentShot = 0;
         // score that player is on in a frame
         $score = 0;
 
-        for ($i = 0; $i <= $this->NumberofPlayers; $i++) {
-            for ($frame = 0; $frame <= $this->NumberofFrames; $frame++){
+        //for each frame, for each player work out what they've bowled
+        //apply the bonus shots and work out score
+        for ($frame = 0; $frame <= $this->NumberofFrames; $frame++){
+            for ($i = 0; $i <= count($this->Players[$i]); $i++) {
                 if($this->Shots[$currentShot] + $this->Shots[$currentShot + 1 ] == 10){
-
-                } else if ($this->Shots[$currentShot] == 10){
-
+                    //spare bonus
+                    $currentShot += 2;
+                } elseif ($this->Shots[$currentShot] == 10){
+                    //strike bonus
+                    $currentShot += 1;
                 } else {
-                    
+                    //regular adding together
+                    $currentShot += 2;
                 }
+                    //return score here for each frame
             }
-        }
 
+        }
 
     }
 
+    function SpareBonusShots(){
+        //record the current shot plus two
+        //two shots as it's a spare then the one after
+    }
+
+    function StrikeBonusShots(){
+        //record the current shot plus one and plus two
+        //both as a strike is just one shot
+    }
 }
